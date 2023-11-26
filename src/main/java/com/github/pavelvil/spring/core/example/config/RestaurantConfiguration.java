e479838b5638c4fa18fa75c70862a06131dfa23f
PavelVil <pavel.vavilov.1994@gmail.com>
2023-11-26 20:52:30 +0300

    Spring beans basic example

---
+++ src/main/java/com/github/pavelvil/spring/core/example/config/RestaurantConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.pavelvil.spring.core.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.pavelvil.spring.core.example.basic")
+public class RestaurantConfiguration {
+}
